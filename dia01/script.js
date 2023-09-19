2616668821ff1684364abc3238262f430c7f6993
Samara-Alves <samara_alves28@hotmail.com>
2023-09-19 10:31:37 -0300

    initial-day01

---
+++ dia01/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function()
+{
+    const changeTextButton = document.getElementById("changeTextButton");
+    const displayText =  document.getElementById("displayText");
+
+    changeTextButton.addEventListener("click", function() {
+        displayText.textContent = "Texto Alterado!";
+    });
+});
