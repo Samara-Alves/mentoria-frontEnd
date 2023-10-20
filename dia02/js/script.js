88098758b7360b83c2c118855b8e5bfdc79e49b2
Samara-Alves <samara_alves28@hotmail.com>
2023-10-20 18:36:32 -0300

    ajustes

--- dia02/js/script.js
+++ dia02/js/script.js
@@ -4,21 +4,19 @@ form.addEventListener('submit', function(event) {
     event.preventDefault();
     
     
-    const weight = document.getElementById('weight');
-    const height = document.getElementById('height');
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
 
+    if (!isNaN(bmi)) {
+        const value = document.getElementById('value');
+        let description = '';
 
     const bmi = (weight / (height * height)).toFixed(2);
 
-
-    const value = document.getElementById('value');
-    let description = '';
-
     document.getElementById('infos').classList.remove('hidden');
 
-/*
     if  (bmi < 18.5) {
-        description = 'Cuidado! Você está abaixo do peso!'
+        description = 'Cuidado! Você está abaixo do peso!';
     }    else if ( bmi >= 18.5 && bmi <= 25 ) {
         description = "Você está no peso ideal";
     }
@@ -35,6 +33,5 @@ form.addEventListener('submit', function(event) {
         description = "Cuidado! você está com obesidade morbida!";
     }
     value.textContent = bmi.replace('.',',');
-    document.getElementById('description').textContent = description;*/
-
+    document.getElementById('description').textContent = description;
 });
